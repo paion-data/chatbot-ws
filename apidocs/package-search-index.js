27c1bbf144233338925610552cfb97acd6a0b8b4
QubitPi <jack20220723@gmail.com>
2025-03-06 10:18:53 +0000

    deploy: a22da331f211f3cfcf87315f09fc2cd1cc4f28e5

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.paiondata.chatbotws.application"},{"l":"com.paiondata.chatbotws.config"},{"l":"com.paiondata.chatbotws.web.endpoints"},{"l":"com.paiondata.chatbotws.web.filters"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.paiondata.chatbotws"},{"l":"com.paiondata.chatbotws.application"},{"l":"com.paiondata.chatbotws.config"},{"l":"com.paiondata.chatbotws.web.endpoints"},{"l":"com.paiondata.chatbotws.web.filters"}];updateSearchResults();
